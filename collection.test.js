5fd163bda8764150d52aaf2a41a2a8b6e9bf4e6d
Mark Eschbach <meschbach@gmail.com>
2019-02-20 18:06:08 -0800

    Ensuring collections do not conflict

--- collection.test.js
+++ collection.test.js
@@ -2,22 +2,87 @@
 function isLoading() {}
 function isLoaded() {}
 
-const ACTION_ITEM_LOADED = ".item-loaded";
-function loadedItem( entity ) {
-	return {
-		type: ACTION_ITEM_LOADED,
-		entity: entity
+function resourceCollection( name ){
+	const ACTION_ITEM_LOADED = name + ".item-loaded";
+	const ACTION_ITEM_LOADING = name + ".item-loading";
+	const ACTION_COLLECTION_LOADING = name + ".collection-loading";
+	const ACTION_COLLECTION_LOADED = name + ".collection-loaded";
+	const ACTION_COLLECTION_IDS_LOADED = name + ".collection-loaded.ids";
+
+	function loadedItem( entity ) {
+		return {
+			type: ACTION_ITEM_LOADED,
+			entity: entity
+		}
 	}
-}
 
-const ACTION_ITEM_LOADING = ".item-loading";
-function loadingItem( id ){
-	return { type: ACTION_ITEM_LOADING,  id };
-}
+	function loadingItem( id ){
+		return { type: ACTION_ITEM_LOADING,  id };
+	}
 
-const ACTION_COLLECTION_LOADING = ".collection-loading";
-function loadingCollection(){
-	return { type: ACTION_COLLECTION_LOADING };
+	function loadingCollection(){
+		return { type: ACTION_COLLECTION_LOADING };
+	}
+
+	function loadedItemIDs( ids ){
+		return { type: ACTION_COLLECTION_IDS_LOADED, ids }
+	}
+
+	function loadedCollection( items ){
+		return {
+			type: ACTION_COLLECTION_LOADED,
+			items
+		};
+	}
+
+	function reducer( state = INIT_STATE, action ){
+		switch (action.type) {
+			case ACTION_ITEM_LOADED: {
+				const itemID = action.entity.id;
+				const updatedState = {state: STATE_LOADED, entity: action.entity };
+				const itemIDChange = {};
+				itemIDChange[itemID] = updatedState;
+				const newItemState = Object.assign({}, state.items, itemIDChange);
+				state = Object.assign({}, state, {items: newItemState});
+			}	break;
+			case ACTION_ITEM_LOADING: {
+				const itemID = action.id;
+				const updatedState = {state: STATE_LOADING};
+				const itemIDChange = {};
+				itemIDChange[itemID] = updatedState;
+				const newItemState = Object.assign({}, state.items, itemIDChange);
+				state = Object.assign({}, state, {items: newItemState});
+			}	break;
+			case ACTION_COLLECTION_LOADING: {
+				state = Object.assign({}, state, {state: STATE_LOADING});
+			}	break;
+			case ACTION_COLLECTION_IDS_LOADED: {
+				const itemIDChanged = action.ids.reduce(function (result, item) {
+					result[item] = { state: STATE_LOADED, entity: {id: item} };
+					return result;
+				}, {});
+				const newItemState = Object.assign({}, state.items, itemIDChanged);
+				state = Object.assign({}, state, {state: STATE_LOADED, items: newItemState});
+			}   break;
+			case ACTION_COLLECTION_LOADED: {
+				const items = action.items.reduce(function (result, entity) {
+					result[entity.id] = { state: STATE_LOADED, entity };
+					return result;
+				}, {});
+				state = Object.assign({}, state, {state: STATE_LOADED, items})
+			}   break;
+		}
+		return state;
+	}
+
+	return {
+		loadedItem,
+		loadingItem,
+		loadedItemIDs,
+		loadingCollection,
+		loadedCollection,
+		reducer
+	};
 }
 
 function isItemLoading( state, id ){
@@ -35,6 +100,11 @@ function isCollectionLoading( state, id ){
 	return false;
 }
 
+function isCollectionLoaded( state ) {
+	if( state.state === STATE_LOADED ) return true;
+	return false;
+}
+
 function selectItem( state, id ){
 	const item = state.items[id];
 	if( !item ) { return ;}
@@ -46,35 +116,12 @@ const TEST_INIT_ACTION = {type: "@@iniitalize"};
 const STATE_LOADED = "loaded";
 const STATE_LOADING = "loading";
 const INIT_STATE = { items: {} };
-function reducer( state = INIT_STATE, action ){
-	switch (action.type) {
-		case ACTION_ITEM_LOADED: {
-			const itemID = action.entity.id;
-			const updatedState = {state: STATE_LOADED, entity: action.entity };
-			const itemIDChange = {};
-			itemIDChange[itemID] = updatedState;
-			const newItemState = Object.assign({}, state.items, itemIDChange);
-			state = Object.assign({}, state, {items: newItemState});
-			break;
-		}
-		case ACTION_ITEM_LOADING:
-			const itemID = action.id;
-			const updatedState = {state: STATE_LOADING};
-			const itemIDChange = {};
-			itemIDChange[itemID] = updatedState;
-			const newItemState = Object.assign({}, state.items, itemIDChange);
-			state = Object.assign({}, state, {items: newItemState});
-			break;
-		case ACTION_COLLECTION_LOADING:
-			state = Object.assign({}, state, {state: STATE_LOADING});
-			break;
-	}
-	return state;
-}
 
 describe( "Given a resource collection", function(){
 	describe("When no actions have been performed against it", function(){
+		const {reducer} = resourceCollection("test-collection");
 		const state = reducer( undefined, TEST_INIT_ACTION);
+
 		test("Then the entire collection is unloaded", function(){
 			expect(isLoaded(state)).toBeFalsy();
 		});
@@ -92,6 +139,7 @@ describe( "Given a resource collection", function(){
 	});
 
 	describe("When an item is loaded",function(){
+		const {reducer, loadedItem } = resourceCollection("check-it");
 		const entity = {id: 0, water:"inland"};
 		const state0 = reducer( undefined, INIT_STATE);
 		const state1 = reducer( state0, loadedItem(entity));
@@ -113,6 +161,7 @@ describe( "Given a resource collection", function(){
 	});
 
 	describe("When an item is loading", function(){
+		const {reducer, loadingItem, loadedItem } = resourceCollection("gated");
 		const id = 2895;
 		const entity = {id, flight:"delayed"};
 		const state0 = reducer( undefined, INIT_STATE);
@@ -136,11 +185,50 @@ describe( "Given a resource collection", function(){
 	});
 
 	describe("When the collection is loading", function(){
+		const { reducer, loadingCollection, loadedItemIDs, loadedCollection } = resourceCollection("hearting");
 		const state0 = reducer( undefined, INIT_STATE);
 		const state1 = reducer( state0, loadingCollection());
 
 		test("Then is loading the collection", function(){
 			expect(isCollectionLoading(state1)).toBeTruthy();
+		});
+
+		describe("And has finished loading the IDs", function () {
+			const ids = [56,26,13,2];
+			const state2 = reducer(state1, loadedItemIDs( ids ));
+
+			test("Then the collection is loaded", function(){
+				expect(isCollectionLoaded(state2)).toBeTruthy();
+			});
+		});
+
+		describe("And the collection is loaded", function(){
+			const state2 = reducer( state0, loadedCollection([]));
+
+			test("Then the collection is loaded", function(){
+				expect(isCollectionLoaded(state2)).toBeTruthy();
+			});
+		});
+	});
+});
+
+const {combineReducers} = require("redux");
+
+describe("Given two collections", function(){
+	const collection0 = resourceCollection("sunbelt");
+	const collection1 = resourceCollection("prestege");
+	const rootReducer = combineReducers({c0: collection0.reducer, c1: collection1.reducer});
+	const state0 = rootReducer( undefined, TEST_INIT_ACTION);
+
+	describe("When one is loaded", function () {
+		const state1 = rootReducer( state0, collection0.loadedCollection([{id:0},{id:1}]) );
+
+		test("Then the collection is loaded", function(){
+			expect(isCollectionLoaded(state1.c0)).toBeTruthy();
+		});
+
+		test("Then the other is not", function(){
+			expect(isCollectionLoaded(state1.c1)).toBeFalsy();
 		});
 	});
 });
