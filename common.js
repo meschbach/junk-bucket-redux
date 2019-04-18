64141884d89470370d4905c929be9c1a59ffa8a7
Mark Eschbach <meschbach@gmail.com>
2019-04-18 09:52:03 -0700

    states: Adds destroying and destroyed

--- common.js
+++ common.js
@@ -3,5 +3,7 @@ module.exports = {
 	STATE_LOADED: "loaded",
 	STATE_LOADING: "loading",
 	STATE_CREATING: "creating",
-	STATE_CREATED: "created"
+	STATE_CREATED: "created",
+	STATE_DESTROYING: "destroying",
+	STATE_DESTROYED: "destroyed"
 };
